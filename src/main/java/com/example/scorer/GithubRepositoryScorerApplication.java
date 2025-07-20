6b8f8dba6bfc726be74c3374fd350fdd07942dee
sree-mandlem <sreeharshamandlem@gmail.com>
2025-07-20 23:09:59 +0200

    Initialize

---
+++ src/main/java/com/example/scorer/GithubRepositoryScorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryScorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryScorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scorer/GithubRepositoryScorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryScorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
